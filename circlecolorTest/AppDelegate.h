3805ac9e2395480ffd1f122cb5a1ed0e06208eed
@jiqiaochun <13011251196@163.com>
2016-08-18 10:03:26 +0800

    Initial Commit

---
+++ circlecolorTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  circlecolorTest
+//
+//  Created by 姬巧春 on 16/8/18.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
